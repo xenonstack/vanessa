2efe00787122b2fe9fa912fd4633877e096f38ed
Abhay Arora <abhayaurora@gmail.com>
2015-06-15 15:58:51 +0529

    Restructured application.

--- core/src/main/scala/com/xenonstack/vanessa/core/Vanessa.scala
+++ core/src/main/scala/com/xenonstack/vanessa/core/Vanessa.scala
@@ -1,37 +1,17 @@
 package com.xenonstack.vanessa.core
 
-import com.xenonstack.vanessa.web.HTTPServer
-//import com.xenonstack.vanessa.web.HTTPSServer
-
-/*
+import com.twitter.finatra.FinatraServer
 import com.xenonstack.vanessa.configuration.Configuration
-import com.xenonstack.vanessa.scheduler.SchedulerDaemon
-*/
-object Vanessa{
 
-	def main(args: Array[String]): Unit = {
+object Vanessa extends FinatraServer{
 
-		if(args.length > 1){
-			if(args(1).equals("http")){
-				val server: HTTPServer = new HTTPServer()
-				server.start()
-			} else{
-				if(args(1).equals("scheduler")){
-					println("FAKE: CRAWL SCHEDULER SERVER STARTED!")
-				} else {
-					if(args(1).equals("https")){
-						println("FAKE: HTTPS server started.")
-					} else{
-						_printUsageInformation()
-					}
-				}
-			}
-		} else{
-			_printUsageInformation()
-		}
+    var conf: Configuration = new Configuration()
+    conf.load(System.getProperty("conf.path").toString())
 
-	}
-
-	def _printUsageInformation(): Unit = println("\n--------------------------\nVanessa 1.0.0 - XenonStack\n--------------------------\n\nUsage: vanessa <command>\n\nAvailable Commands :\n\thttp\t\tStarts vanessa HTTP server.\n\thttps\t\tStarts vanessa HTTPS server.\n\tscheduler\tStarts vanessa crawl scheduler server.\n\n")
+    System.setProperty("com.twitter.finatra.config.adminPort", ":8888");
+    System.setProperty("com.twitter.finatra.config.appName", "vanessa");
+    System.setProperty("com.twitter.finatra.config.env", "production");
+    System.setProperty("com.twitter.finatra.config.logNode", "vanessa");
+    System.setProperty("com.twitter.finatra.config.port", ":" + conf.get("server.port").toString());
 
 }
